a0552cf205dc15f9472bcc6a035c3da6348ad53c
ViniciusSimulacro <delucca86@gmail.com>
2023-02-28 20:10:14 -0300

    add input function and first response

--- ibge.py
+++ ibge.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-nome = 'vinicius' #input(str('Qual o nome você quer consultar: '))
+nome = input(str('Qual o nome você quer consultar: '))
 request = requests.get(f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}')
 request = request.json()
 periodos = (request[0]['res'])
@@ -9,4 +9,4 @@ data = (periodos[1]['periodo'])
 inicio = data[1:5]
 fim = data[6:10]
 quantidade = (periodos[1]['frequencia'])
-print(quantidade)
+print(f'No período de {inicio} até {fim} o nome {nome} tem {quantidade} registros!')
