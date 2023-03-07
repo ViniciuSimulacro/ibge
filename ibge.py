bc17ed608444ad3c7863f13cc00bce68833e8edc
ViniciusSimulacro <delucca86@gmail.com>
2023-03-06 22:40:29 -0300

    doing a for to print all values

--- ibge.py
+++ ibge.py
@@ -5,8 +5,14 @@ nome = input(str('Qual o nome você quer consultar: '))
 request = requests.get(f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}')
 request = request.json()
 periodos = (request[0]['res'])
-data = (periodos[1]['periodo'])
-inicio = data[1:5]
-fim = data[6:10]
-quantidade = (periodos[1]['frequencia'])
-print(f'No período de {inicio} até {fim} o nome {nome} tem {quantidade} registros!')
+total = len(periodos)
+for n in range(0,total):
+    data = (periodos[n]['periodo'])
+    inicio = data[1:5]
+    fim = data[6:10]
+    quantidade = (periodos[n]['frequencia'])
+    if inicio == '930[':
+        print(f'No ano de 1930 nome {nome} teve {quantidade} registros!')
+    else:
+        print(f'No período de {inicio} até {fim} o nome {nome} teve {quantidade} registros!')
+    
